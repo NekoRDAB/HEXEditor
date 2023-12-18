e83868fd27dcc28c3106ae80993746343f09a689
Danil <ferrum268@mail.ru>
2023-12-18 17:25:19 +0500

    rewrite change()

--- byte_change.py
+++ byte_change.py
@@ -1,12 +1,13 @@
 from dataclasses import dataclass
 from HexFile import HexFile
 
+
 @dataclass
 class ByteChange:
     position: int
     value: str
 
 
-def apply_changes(hex_file : HexFile, changes : list):
+def apply_changes(hex_file: HexFile, changes: list):
     for change in changes:
         hex_file.change_byte_at(change.position, int(change.value, 16))
